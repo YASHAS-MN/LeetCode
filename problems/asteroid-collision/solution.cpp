93f1f92df1c6e60f985d105db4d5db56c0ab36d9
Yashas M N <138572042+YASHAS-MN@users.noreply.github.com>
2026-08-17 01:07:38 +0530

    feat: add LeetCode 735. Asteroid Collision solution (cpp)

---
+++ problems/asteroid-collision/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> st;
+
+        for(int ast: asteroids)
+        {
+            bool alive = true;
+
+            while(alive && !st.empty() && st.back()>0 && ast<0)
+            {
+                if(st.back()< -ast)
+                st.pop_back();
+
+                else if(st.back() == -ast)
+                {
+                    st.pop_back();
+                    alive = false;
+                }
+
+                else
+                alive = false;
+            }
+            
+            if(alive)
+            st.push_back(ast);
+
+        }
+        return st;
+    }
+};
