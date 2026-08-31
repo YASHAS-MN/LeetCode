893e89124f78fc930629aa1100fce13dd03777af
Yashas M N <138572042+YASHAS-MN@users.noreply.github.com>
2026-08-31 12:33:10 +0530

    feat: add LeetCode 560. Subarray Sum Equals K solution (cpp)

---
+++ problems/subarray-sum-equals-k/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        int sum =0, count=0;
+
+        freq[0] = 1;
+
+        for(int num: nums){
+            sum+=num;
+
+            if(freq.count(sum-k))
+             count+=freq[sum-k];
+
+            freq[sum]++;
+        }
+        return count;
+    }
+};
