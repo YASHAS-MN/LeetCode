0c2e89cf290d08ef066a7d6d0e2dd66907148d5d
Yashas M N <138572042+YASHAS-MN@users.noreply.github.com>
2026-09-01 15:48:37 +0530

    feat: update LeetCode 268. Missing Number solution (cpp)

--- problems/missing-number/solution.cpp
+++ problems/missing-number/solution.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size();
+        /*int n = nums.size();
 
         for(int i=0; i<nums.size(); i++)
         n^=i^nums[i];
-        return n;
+        return n;*/
+
+        int n = nums.size();
+
+        int expected = n*(n+1)/2;
+        int actual = 0;
+
+        for(int num : nums)
+            actual+=num;
+
+        return expected-actual;
     }
 };
